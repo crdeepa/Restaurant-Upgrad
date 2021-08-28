b873a42215ff25b653942fa313f3d6e5ce2a995b
crdeepa <crdeepa@gmail.com>
2021-08-28 15:26:04 +0530

    Formatting

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -12,9 +12,11 @@ class RestaurantTest {
     @BeforeEach
 
     public void setup() {
-        LocalTime openingTime=LocalTime.parse("09:00:00");
-        LocalTime closingTime=LocalTime.parse("23:00:00");
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
         restaurant=new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
     }
 
 
@@ -25,7 +27,7 @@ class RestaurantTest {
         //WRITE UNIT TEST CASE HERE
         Restaurant spyRestaurant=Mockito.spy(restaurant);
 
-        Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(LocalTime.parse("09:01:00"));
+        Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(LocalTime.parse("10:31:00"));
 
         assertTrue(spyRestaurant.isRestaurantOpen());
     }
@@ -35,7 +37,7 @@ class RestaurantTest {
         //WRITE UNIT TEST CASE HERE
         Restaurant spyRestaurant=Mockito.spy(restaurant);
 
-        Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(LocalTime.parse("08:59:00"));
+        Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(LocalTime.parse("10:29:00"));
 
         assertFalse(spyRestaurant.isRestaurantOpen());
 
@@ -47,36 +49,20 @@ class RestaurantTest {
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void adding_item_to_menu_should_increase_menu_size_by_1(){
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.addToMenu("Sizzling brownie",319);
         assertEquals(initialMenuSize+1,restaurant.getMenu().size());
     }
+
     @Test
     public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.removeFromMenu("Vegetable lasagne");
         assertEquals(initialMenuSize-1,restaurant.getMenu().size());
     }
+
     @Test
     public void removing_item_that_does_not_exist_should_throw_exception() {
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-
         assertThrows(itemNotFoundException.class,
                 ()->restaurant.removeFromMenu("French fries"));
     }
