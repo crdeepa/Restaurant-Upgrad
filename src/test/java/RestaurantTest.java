048097a8d6618a71b5ebbb7cf2114630872996b6
crdeepa <crdeepa@gmail.com>
2021-08-28 15:09:03 +0530

    Modified RestaurantTest.java

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalTime;
 
@@ -8,17 +9,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class RestaurantTest {
     Restaurant restaurant;
     //REFACTOR ALL THE REPEATED LINES OF CODE
+    @BeforeEach
+
+    public void setup() {
+        LocalTime openingTime=LocalTime.parse("09:00:00");
+        LocalTime closingTime=LocalTime.parse("23:00:00");
+        restaurant=new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+    }
+
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
+        Restaurant spyRestaurant=Mockito.spy(restaurant);
+
+        Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(LocalTime.parse("09:01:00"));
+
+        assertTrue(spyRestaurant.isRestaurantOpen());
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
+        Restaurant spyRestaurant=Mockito.spy(restaurant);
+
+        Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(LocalTime.parse("08:59:00"));
+
+        assertFalse(spyRestaurant.isRestaurantOpen());
 
     }
 
